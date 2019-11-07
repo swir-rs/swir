20b3d78afba86d4903f17f3d39b722bb274ca703
Dawid Nowak <nowakd@gmail.com>
2019-11-07 21:23:01 +0000

    Fixing lack of HTTP body and other stuff.

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -172,45 +172,51 @@ pub fn kafka_event_handler(rx: &Receiver<Message>, kafka_producer: &mut Producer
 }
 
 pub fn kafka_incoming_event_handler(iter: MessageSetsIter, client: &Client<HttpConnector, Body>, consumer: &mut Consumer, db: &Db) {
-    for ms in iter {
-        let topic = ms.topic();
-        let mut uri: String = String::from("");
-        if let Ok(maybe_url) = db.get(topic) {
-            if let Some(url) = maybe_url {
-                let vec = url.to_vec();
-                let b = vec.as_bytes();
-                uri = String::from_utf8_lossy(b).to_string();
+    let f = {
+        for ms in iter {
+            let topic = ms.topic();
+            let mut uri: String = String::from("");
+            if let Ok(maybe_url) = db.get(topic) {
+                if let Some(url) = maybe_url {
+                    let vec = url.to_vec();
+                    let b = vec.as_bytes();
+                    uri = String::from_utf8_lossy(b).to_string();
+                }
             }
-        }
 
-        for m in ms.messages() {
-            let kafka_msg = String::from_utf8_lossy(m.value);
-            info!("Received message {:?}", kafka_msg);
-            let b = Body::empty();
-            let mut postreq = Request::new(Body::empty());
-            *postreq.method_mut() = Method::POST;
-            *postreq.uri_mut() = Uri::from(uri.parse().unwrap_or_default());
-            postreq.headers_mut().insert(
-                hyper::header::CONTENT_TYPE,
-                HeaderValue::from_static("application/json"),
-            );
-            let cl = b.content_length().unwrap().to_string();
-            postreq.headers_mut().insert(
-                hyper::header::CONTENT_LENGTH,
-                HeaderValue::from_str(&cl).unwrap(),
-            );
-            let post = client.request(postreq).and_then(|res| {
-                info!("POST: {}", res.status());
-                res.into_body().concat2()
-            }).map(|_| {
-                info!("Done.");
-            }).map_err(|err| {
-                warn!("Error {}", err);
-            });
-            hyper::rt::run(post);
+            for m in ms.messages() {
+                let kafka_msg = String::from_utf8_lossy(m.value).to_string();
+                info!("Received message {:?}", kafka_msg);
+
+                let mut postreq = Request::new(Body::empty());
+                *postreq.method_mut() = Method::POST;
+                *postreq.uri_mut() = Uri::from(uri.parse().unwrap_or_default());
+                postreq.headers_mut().insert(
+                    hyper::header::CONTENT_TYPE,
+                    HeaderValue::from_static("application/json"),
+                );
+
+                *postreq.body_mut() = Body::from(kafka_msg);
+                let cl = postreq.body_mut().content_length().unwrap().to_string();
+                postreq.headers_mut().insert(
+                    hyper::header::CONTENT_LENGTH,
+                    HeaderValue::from_str(&cl).unwrap(),
+                );
+                let post = client.request(postreq).and_then(|res| {
+                    info!("POST: {}", res.status());
+                    res.into_body().concat2()
+                }).map(|_| {
+                    info!("Done.");
+                }).map_err(|err| {
+                    warn!("Error {}", err);
+                });
+                hyper::rt::run(post);
+            }
+            let r = consumer.consume_messageset(ms);
+            info!("Consumed result {:?}", r);
         }
-        let r = consumer.consume_messageset(ms);
-        info!("Consumed result {:?}", r);
-    }
-    consumer.commit_consumed().unwrap();
+        consumer.commit_consumed().unwrap();
+        future::ok(())
+    };
+    hyper::rt::run(f);
 }
