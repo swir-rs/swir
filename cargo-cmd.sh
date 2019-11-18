b78c669b15d065e41694c4ac4605298c07c2c6bf
Dawid Nowak <nowakd@gmail.com>
2019-11-18 21:32:26 +0000

    Cargo commands to build and run

--- cargo-cmd.sh
+++ cargo-cmd.sh
@@ -1,7 +1,7 @@
 #For kafka
 cargo build
-cargo run --package rustycar --bin rustycar -- -a 127.0.0.1  -b 127.0.0.1:9092 -s Request -r Response -g rustycar
+export RUST_LOG=info; export RUST_BACKTRACE=full;  cargo run --package rustycar --bin rustycar -- -a 127.0.0.1  -b 127.0.0.1:9092 -s Request -r Response -g rustycar
 
 #For NATS
 cargo build build --features="with_nats"
-cargo run --features "with_nats" --package rustycar --bin rustycar -- -a 127.0.0.1  -b 127.0.0.1:4222 -s Request -r Response -g rustycar
+cargo RUST_LOG=info; export RUST_BACKTRACE=full; run --features "with_nats" --package rustycar --bin rustycar -- -a 127.0.0.1  -b 127.0.0.1:4222 -s Request -r Response -g rustycar
