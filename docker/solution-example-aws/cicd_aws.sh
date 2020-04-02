624db7eb10ca7bebfe3c82d1a4fba60260425fc2
Dawid Nowak <nowakd@gmail.com>
2020-04-02 13:40:00 +0100

    Adding persistence abstraction layer.
    * Fixing README and bumping up version number
    * Fixing scripts again
    * Fixing diagrams
    
     Changes to be committed:
    	modified:   Cargo.toml
    	new file:   README.html
    	modified:   README.md
    	new file:   graphics/swir_architecture.png

--- docker/solution-example-aws/cicd_aws.sh
+++ docker/solution-example-aws/cicd_aws.sh
@@ -17,23 +17,43 @@ cd docker/solution-example-aws
 source ../../secure.sh
 
 cd ./swir-configurator
+printf "\n**********************\n"
+printf "\nConfigurator \n"
 docker build --tag swir-aws-example-configurator:v2 .
+printf "\nConfigurator... done"
+printf "\n**********************\n"
 
 cd ../../solution-example/swir-python-processor
+printf "\n**********************\n"
+printf "\nPython processor  \n"
 ./build.sh
 docker build --tag swir-example-python-processor:v2 .
+printf "\nPython processor  done"
+printf "\n**********************\n"
 
 cd ../swir-java-processor
+printf "\n**********************\n"
+printf "\nJava processor  \n"
 ./gradlew clean bootJar
 docker build --tag swir-example-java-processor:v2 .
+printf "\nJava processor  done"
+printf "\n**********************\n"
 
 cd ../swir-java-source
+printf "\n**********************\n"
+printf "\nJava GRPC source  \n"
 ./gradlew clean build installDist assembleDist
 docker build --tag swir-example-java-source:v2 .
+printf "\nJava GRPC source...done"
+printf "\n**********************\n"
 
 cd ../swir-python-sink
+printf "\n**********************\n"
+printf "\nPython GRPC sink  \n"
 ./build.sh
 docker build --tag swir-example-python-sink:v2 .
+printf "\nPython GRPC sink... done  \n"
+printf "\n**********************\n"
 
 cd ../../solution-example-aws/
 cd aws-scripts
