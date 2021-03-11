de9e93f16da53a720fcae647376b108a39c385ba
Dawid Nowak <nowakd@gmail.com>
2021-03-11 15:18:35 +0000

    Bumping tracing deps

--- src/utils/tracing_utils.rs
+++ src/utils/tracing_utils.rs
@@ -1,9 +1,13 @@
 use super::config::*;
-use opentelemetry::{
-    api,
-    api::{context::propagation::text_propagator::HttpTextFormat, Provider},
-    sdk,
+use opentelemetry::global;
+use opentelemetry::propagation::TextMapPropagator;
+use opentelemetry::sdk::{
+    propagation::TraceContextPropagator,
+    trace,
+    trace::{IdGenerator, Sampler},
+    Resource,
 };
+
 use std::collections::HashMap;
 use tracing::Span;
 use tracing_opentelemetry::OpenTelemetrySpanExt;
@@ -15,9 +19,9 @@ use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, Env
 pub fn get_tracing_header() -> Option<(&'static str, String)> {
     let span = Span::current();
     let span_ctx = span.context();
-    let propagator = api::TraceContextPropagator::new();
+    let tc_propagator = TraceContextPropagator::new();
     let mut carrier = std::collections::HashMap::new();
-    propagator.inject_context(&span_ctx, &mut carrier);
+    tc_propagator.inject_context(&span_ctx, &mut carrier);
     carrier.get("traceparent").map(|f| ("traceparent", f.to_string()))
 }
 
@@ -29,11 +33,11 @@ pub fn from_http_headers(span: Span, header_map: &HeaderMap) -> Span {
     let trace_header = HeaderName::from_lowercase("traceparent".as_bytes()).unwrap();
 
     if let Some(value) = header_map.get(trace_header) {
-        let propagator = api::TraceContextPropagator::new();
+        let propagator = TraceContextPropagator::new();
         let mut carrier = std::collections::HashMap::new();
         carrier.insert("traceparent".to_string(), String::from_utf8_lossy(value.as_bytes()).to_string());
         let ctx = propagator.extract(&carrier);
-        span.set_parent(&ctx);
+        span.set_parent(ctx);
         span
     } else {
         span
@@ -41,18 +45,18 @@ pub fn from_http_headers(span: Span, header_map: &HeaderMap) -> Span {
 }
 
 pub fn from_bytes(span: Span, value: &[u8]) -> Span {
-    let propagator = api::TraceContextPropagator::new();
+    let propagator = TraceContextPropagator::new();
     let mut carrier = std::collections::HashMap::new();
     carrier.insert("traceparent".to_string(), String::from_utf8_lossy(value).to_string());
     let ctx = propagator.extract(&carrier);
-    span.set_parent(&ctx);
+    span.set_parent(ctx);
     span
 }
 
 pub fn from_map(span: Span, map: &HashMap<String, String>) -> Span {
-    let propagator = api::TraceContextPropagator::new();
+    let propagator = TraceContextPropagator::new();
     let ctx = propagator.extract(map);
-    span.set_parent(&ctx);
+    span.set_parent(ctx);
     span
 }
 
@@ -65,21 +69,24 @@ pub fn init_tracer(config: &Swir) -> Result<(), Box<dyn std::error::Error>> {
     if let Some(cfg) = &config.tracing {
         if let Some(open_telemetry) = &cfg.open_telemetry {
             debug!("Open telementry tracing selected {:?}", open_telemetry);
-            let exporter = opentelemetry_jaeger::Exporter::builder()
-                .with_agent_endpoint(format!("{}:{}", open_telemetry.collector_address, open_telemetry.collector_port).parse().unwrap())
-                .with_process(opentelemetry_jaeger::Process {
-                    service_name: open_telemetry.service_name.clone(),
-                    tags: Vec::new(),
-                })
-                .init()?;
-            let provider = sdk::Provider::builder()
-                .with_simple_exporter(exporter)
-                .with_config(sdk::Config {
-                    default_sampler: Box::new(sdk::Sampler::AlwaysOn),
-                    ..Default::default()
-                })
-                .build();
-            let tracer = provider.get_tracer("tracing");
+
+            global::set_text_map_propagator(opentelemetry_jaeger::Propagator::new());
+            let (tracer, _uninstall) = opentelemetry_jaeger::new_pipeline()
+                .from_env()
+                .with_agent_endpoint(format!("{}:{}", open_telemetry.collector_address, open_telemetry.collector_port))
+                .with_service_name(&open_telemetry.service_name)
+                .with_tags(vec![])
+                .with_trace_config(
+                    trace::config()
+                        .with_default_sampler(Sampler::AlwaysOn)
+                        .with_id_generator(IdGenerator::default())
+                        .with_max_events_per_span(64)
+                        .with_max_attributes_per_span(16)
+                        .with_max_events_per_span(16)
+                        .with_resource(Resource::new(vec![])),
+                )
+                .install()?;
+
             let opentelemetry = tracing_opentelemetry::layer().with_tracer(tracer);
             let registry = registry.with(opentelemetry);
             if let Err(e) = registry.try_init() {
